6f601134364fbba15f834289c6e1d7e860ed0034
apy122 <apy122@gmail.com>
2025-11-02 10:15:29 +0100

    Creacion clase usuario, creacion del modal con js e inserccion en la web y creacion manejo archivos

--- Backend/utils/manejoArchivos.js
+++ Backend/utils/manejoArchivos.js
@@ -1,1 +1,9 @@
-//Aqui va la logica de json general
+import fs from "fs";
+import path from "path";
+const ruta= "C:\\Backend\\data\\usuarios.json"
+
+export function leerUsuarios() {
+  if (!fs.existsSync(ruta)) return [];
+  const data = fs.readFileSync(ruta, "utf-8") || "[]";
+  return JSON.parse(data);
+}
