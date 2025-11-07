2974420f21ce0a188f92a21dee8c75a05a794787
apy122 <apy122@gmail.com>
2025-11-07 16:59:54 +0100

    Nuevos cambios en backend. modificado el puesto model y  anadido funciones vacias del puestoController

--- Backend/controllers/puestoController.js
+++ Backend/controllers/puestoController.js
@@ -1,1 +1,48 @@
+import PuestoModel from '../models/PuestoModel.js';
+//Funciones del puesto
+export async function crearPuesto() {
+    
+}
+export async function listarPuestos() {
+    
+}
+export async function buscarPuestoNombre(nombrePuesto) {
+    //Para que el usuario busque por nombre el puesto   
+}
+export async function buscarPuestoId(idPuesto) {
+    
+}
+export async function buscarPuestoCategoria(categoriaPuesto) {
+    
+}
 
+//Funciones Producto
+export async function agregarProducto() {
+    
+}
+export async function listarProductos() {
+    
+}
+export async function buscarProductoId(idProducto) {
+    //Aq
+}
+export async function buscarProductonombre(nombreProducto) {
+    //Para que el usuario Busque por nombre
+}
+export async function eliminarProductos(idProductos) {
+    
+}
+
+//Gestion Turnos
+export async function asignarTurno() {
+    
+}
+export async function sumarTurno() {
+    
+}
+export async function obtenerTurno() {
+    
+}
+export async function TurnosFaltantes(usuario) {
+    //Aqui para notificar turnos
+}
