c601770165b4d6bcb8fb6039cb5d5e3e81ef0ed0
apy122 <apy122@gmail.com>
2025-11-07 20:49:10 +0100

    varias cosas

--- Backend/app.js
+++ Backend/app.js
@@ -13,4 +13,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
