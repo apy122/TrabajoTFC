4d060ae350d67b44df0cbfd226f64ad2f2144aab
apy122 <apy122@gmail.com>
2025-12-09 22:28:11 +0100

    otro cambio

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,26 @@
 // Importar dependencias
 import express from "express";
 import cors from "cors";
-import  dotenv from "dotenv";
-import conexion from "./database/Conexion.js";
+import dotenv from "dotenv";
+import { conexion } from "./database/Conexion.js";
 import PuestoRoutes from "./routes/PuestoRoutes.js";
 import router from "./routes/PuestoRoutes.js";
 
 // Ejecutar la configuración de dotenv para cargar el .env
 dotenv.config();
 // conexion a donde toca
-conexion();
+try {
+  await conexion.authenticate();
+  console.log("Conexion establecida");
+} catch (error) {
+  console.error("Error en la conexion: " + error);
+}
 
 // Crear servidor
 const app = express();
 
 // Acceder al puerto usando process.env.PUERTO
-const port = process.env.puerto
+const port = process.env.puerto;
 
 // Configurar CORS
 app.use(cors());
@@ -24,9 +29,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Endpoint de prueba real
-app.use('/puestos/puestosZona', PuestoRoutes.router);
+app.use("/puestos/puestosZona", PuestoRoutes.router);
 
 // Servidor en escucha
 app.listen(port, () => {
